2c06bd1c976c5dacddb4eec72b95b718ea4b9e54
Petr Mikhailov <mpshigh@gmail.com>
2021-04-14 21:10:22 +0300

    added day 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,35 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+        long start = System.currentTimeMillis();
+        String viaStringBuilder = generateStringInSpecifiedRangeWithStringBuilder(0, 20000);
+        long end = System.currentTimeMillis();
+        System.out.println(viaStringBuilder);
+
+
+        long start2 = System.currentTimeMillis();
+        String viaString = generateStringInSpecifiedRangeWithString(0, 20000);
+        long end2 = System.currentTimeMillis();
+        System.out.println(viaString);
+
+        System.out.println("Замер времени работы программы через StringBuilder " + (end - start));
+        System.out.println("Замер времени работы программы через String " + (end2 - start2));
+    }
+
+
+    static String generateStringInSpecifiedRangeWithStringBuilder(int initial, int end) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = initial; i < end + 1; i++) {
+            sb.append(i + " ");
+        }
+        return sb.toString();
+    }
+
+    static String generateStringInSpecifiedRangeWithString(int initial, int end) {
+        String string = new String();
+        for (int i = initial; i < end + 1; i++) {
+            string = string + " " + i;
+        }
+        return string;
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,8 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Airplane airplane = new Airplane("Boing", 2020, 100, 10000);
+        airplane.info();
+        System.out.println(airplane.toString());
     }
 }
